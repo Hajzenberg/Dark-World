142c9f24011b84a2c72c2dd54115be6cec131d50
Hajzenberg <kojadinovicdragoljub@gmail.com>
2017-11-04 13:15:23 +0100

    - Removed unnecessary fields from Background class

--- src/com/tfs/darkworld/entities/Background.java
+++ src/com/tfs/darkworld/entities/Background.java
@@ -12,25 +12,13 @@ public class Background extends GameEntity {
 	private static int SKY_OFFSET = 100;
 	
 	private Rectangle mGroundRect;
-	private Rectangle mSkyRect;
-	
-	private BufferedImage mMountainImage;
-	private BufferedImage mSkyImage;
 	
 	private double mScale;
 
 	public Background(int sw, int sh) {
 		super(0, 0, 0, 0, 0, 0);
-		mSkyRect = new Rectangle(0, 0, sw, sh);
 		mGroundRect = new Rectangle(0, sh - 100, sw, sh);
 		mScale = 0.2;
-		mMountainImage = CommonRasters.getMountainBackground();
-		if (mMountainImage == null){
-			System.out.println("image je null");
-		} else {
-			System.out.println("image nije null");
-		}
-		//mSkyImage = CommonRasters.getLightBackground();
 	}
 
 	@Override
